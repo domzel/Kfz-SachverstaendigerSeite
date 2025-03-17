84c1283ace34749f695641e0e4be57f726691248
Dominik Zelder <dominik.zelder@stud.htwk-leipzig.de>
2025-03-17 17:22:53 +0100

    feat(): Seite Unfall was tuen weiter gemacht.

--- KfzSeite/pages/unfall/wastuen.vue
+++ KfzSeite/pages/unfall/wastuen.vue
@@ -1,6 +1,79 @@
 <template>
-<p> Unfall was tuen</p>
+  <section class="container mx-auto mt-10 flex flex-col lg:flex-row items-start gap-10 p-5 max-w-screen-lg">
+    <article class="flex-1">
+      <h2 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-center text-black">
+        Unfall - Was tun?
+      </h2>
+      <br />
+      <div class="bg-white shadow-lg rounded-lg p-6 w-full">
+        <h2 class="text-xl font-semibold text-black mb-4">Haftpflichtschaden</h2>
+        <div class="text-black">
+          <p>Wie verhalte ich mich nach einem Autounfall?</p>
+          <br />
+          <p>
+            Nach dem Autounfall gilt: Ruhe bewahren. Erste Hilfe leisten, falls Personen zu Schaden gekommen sind.
+            Unabhängig davon, ob der Unfall fremd- oder selbstverschuldet war, ist der Schreck unmittelbar nach dem
+            Unfall groß, und man fühlt sich oft hilflos.
+          </p>
+          <br />
+          <p>
+            Wenn bei einem Autounfall keine Personen zu Schaden gekommen sind, versuchen Sie besonders bei einer
+            unklaren Schuldfrage, den Verlauf des Unfallgeschehens zu dokumentieren. Besonders wichtig ist die Stellung der am Unfall beteiligten Fahrzeuge, um bei einer Unfallrekonstruktion den
+            genauen Ablauf zu beweisen. Sollte die Möglichkeit bestehen, empfiehlt es sich, die Fahrzeuge in
+            Unfallposition zu belassen oder zumindest zu fotografieren. Anhand von Bildern, Kollisionswinkeln
+            und den Beschädigungen an den Fahrzeugen ist es erfahrenen Gutachtern später möglich, einen
+            detaillierten Ablauf des Unfalls zu rekonstruieren.
+          </p>
+          <br />
+          <p>
+            Sollten Sie unsicher sein, ob Sie eine Mitschuld oder alleinige Schuld an dem Unfall haben, äußern Sie diese Meinung nicht, bezahlen kein Verwarnungsgeld an die Polizei und unterschreiben
+            Sie vor allem keine derartig lautenden Erklärungen am Unfallort. Eine einmal
+            getätigte Unterschrift/Erklärung lässt sich schwer widerlegen.
+          </p>
+          <br />
+          <p>
+            Auch bei vermeintlichen Bagatellschäden empfiehlt es sich, die Polizei zu informieren, selbst wenn
+            der Unfallgegner ein Verschulden seinerseits einräumt.
+          </p>
+          <br />
+          <p>
+            Sollte der Eigentümer des beschädigten Fahrzeugs nicht anwesend sein, verlassen Sie nicht einfach den
+            Unfallort! Sie haben eine Wartepflicht! Ein Zettel an der Windschutzscheibe reicht nicht aus. Sie
+            sollten die Polizei über den Unfall informieren. Kommen Sie Ihrer Meldepflicht nicht nach, machen Sie
+            sich evtl. nach § 142 StGB der Unfallflucht strafbar.
+          </p>
+          <br />
+          <p>
+            Achten Sie auf Fahrzeuge mit Insassen oder Personen an der Unfallstelle, die den Unfall evtl. gesehen
+            haben und als Zeugen nützlich sein könnten. Lassen Sie sich deren Personalien geben bzw.
+            erfassen Sie Kennzeichen der Fahrzeuge.
+          </p>
+        </div>
+      </div>
+    </article>
+    <div class="w-96 flex-shrink-0">
+      <div class="card card-compact bg-white shadow-xl">
+        <div class="card-body">
+          <div class="w-8 h-8 items-center justify-center">
+            <img src="/images/telefonIcon.png" alt="Telefon Icon" />
+          </div>
+          <h3 class="card-title text-xl text-black">Telefon:</h3>
+          <p class="text-xl text-black">02104/5043939</p>
+          <br />
+          <div class="w-8 h-8 items-center justify-center">
+            <img src="/images/handyIcon.png" alt="Handy Icon" />
+          </div>
+          <h3 class="card-title text-xl text-black">Mobil:</h3>
+          <p class="text-xl text-black">0172 9999 006</p>
+        </div>
+        <figure>
+          <img src="/images/damianDraussen.jpg" alt="Felge" class="w-full" />
+        </figure>
+      </div>
+    </div>
+  </section>
 </template>
+
 
 <script>
 export default {
