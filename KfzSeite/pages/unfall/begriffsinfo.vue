d87cd51d98b672fe0a094abf0c19545b5d8bc77f
Dominik Zelder <dominik.zelder@stud.htwk-leipzig.de>
2025-03-17 17:10:42 +0100

    feat(): Seite Begriffsinfos weiter gemacht.

--- KfzSeite/pages/unfall/begriffsinfo.vue
+++ KfzSeite/pages/unfall/begriffsinfo.vue
@@ -1,5 +1,238 @@
 <template>
-<p>BegriffsInfo</p>
+<section class="container lg:flex mx-auto mt-10 overflow-hidden p-5">
+  <article class="container flex flex-col items-center mt-10">
+    <h2
+        class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-center text-black">
+      Begriffsinformationen
+    </h2>
+    <p class="mb-6 text-lg font-normal text-black lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Haftpflichtschaden
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Im Haftpflichtschadenfall ist der Unfallverursacher gesetzlich verpflichtet, dem Geschädigten
+            gemäß § 249 BGB den unfallbedingten Schaden zu ersetzen. Der Geschädigte muss so gestellt werden,
+            als wäre der Unfall nicht eingetreten. Dies umfasst sowohl materielle Schäden (z. B. Reparaturkosten,
+            Wiederbeschaffungswert, Mietwagenkosten) als auch immaterielle Schäden (z. B. Schmerzensgeld
+            bei Personenschäden).
+          </p>
+          <br />
+          <p>
+            Nach § 3 des Pflichtversicherungsgesetzes (PflVG) tritt an die Stelle des Schädigers die
+            Kfz-Haftpflichtversicherung des Unfallverursachers. Diese übernimmt die Regulierung der
+            berechtigten Schadenersatzansprüche des Geschädigten. Dabei werden insbesondere Kosten für
+            Reparaturen, Wertminderung, Nutzungsausfall und gegebenenfalls weitere Schadenspositionen erstattet.
+          </p>
+          <br />
+          <p>
+            Beim Haftpflichtschadenfall handelt es sich um eine <strong>zivilrechtliche Schadenersatzforderung</strong>,
+            die <strong>außergerichtlich</strong> oder <strong>gerichtlich</strong> geltend gemacht werden kann.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Kaskoschaden
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Im Kaskoschadenfall hat der Versicherungsnehmer bei einem selbst verschuldeten Unfall oder bei
+            bestimmten anderen versicherten Ereignissen (z. B. Diebstahl, Elementarschäden, Wildunfälle)
+            Anspruch auf Ersatz der unfallbedingten Schäden gemäß den Bedingungen seiner Kaskoversicherung.
+            Es handelt sich hierbei ausschließlich um vertragliche Ansprüche aus dem Versicherungsvertrag,
+            die strikt von den gesetzlichen Schadenersatzansprüchen im Haftpflichtschadenfall zu unterscheiden sind.
+          </p>
+          <br />
+          <p>
+            Die Höhe der Ersatzleistung richtet sich nach den jeweils geltenden Kaskobedingungen.
+            In der Regel trägt der Versicherungsnehmer eine vertraglich vereinbarte Selbstbeteiligung,
+            die von der Entschädigungssumme abgezogen wird. Zudem kann die Regulierung des Schadens
+            Auswirkungen auf den Schadenfreiheitsrabatt haben, wodurch sich die zukünftigen Versicherungsbeiträge
+            erhöhen können.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Totalschaden
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Von einem Totalschaden spricht man, wenn die Wiederherstellung des beschädigten
+            Fahrzeuges entweder nicht möglich (technischer Totalschaden) oder unwirtschaftlich ist
+            (wirtschaftlicher Totalschaden).
+          </p>
+          <br />
+          <p>
+            Der Anspruch auf Wiederherstellung verwandelt sich dann in einen Anspruch auf Geldersatz.
+          </p>
+          <br />
+          <p>
+            Technischer Totalschaden liegt vor bei völliger Zerstörung des Fahrzeugs oder bei Unmöglichkeit
+            der Reparatur aus technischen Gründen.
+          </p>
+          <br />
+          <p>
+            Der wirtschaftliche Totalschaden liegt vor, wenn unter Berücksichtigung der wirtschaftlichen
+            Gegebenheiten nicht mehr von Reparaturwürdigkeit gesprochen werden kann.
+          </p>
+          <br />
+          <p>
+            Von einem unechten Totalschaden spricht man, wenn dem Geschädigten die Reparatur nicht zugemutet
+            werden kann, obwohl die Summe aus Minderwert und Reparaturkosten geringer ist als die Differenz
+            zwischen Wiederbeschaffung und Restwert.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Nutzungsausfall
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Der Geschädigte, der kein Ersatzfahrzeug anmietet, hat grundsätzlich Anspruch auf Geldentschädigung
+            im Sinne von § 249 Abs. 2 BGB für die Entziehung der Nutzungsmöglichkeit seines beschädigten Pkw`s.
+            Die Höhe der Nutzungsausfallentschädigung bemisst sich u. a. nach der Reparaturdauer. Der konkrete
+            Tagessatz kann bspw. der Nutzungsausfallentschädigungstabelle „Sanden, Danne, Küppersbusch“ entnommen
+            werden. Der Kfz-Sachverständige wird im Schadengutachten die technische Einordnung des Fahrzeuges für
+            den Nutzungsausfall vornehmen.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Wiederbeschaffungswert
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Der Wiederbeschaffungswert ist der Wert, den der Geschädigte für ein vergleichbares Fahrzeug
+            bei einem seriösen Händler aufwenden muss. Der Sachverständige berücksichtigt bei der Ermittlung
+            des Wiederbeschaffungswertes alle wertbildenden Faktoren sowie die örtliche Marktlage.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Restwert
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Zur Definition des Restwertes hat der BGH bereits am 04.06.1993 entschieden, dass der Geschädigte
+            bei Ausübung der Ersetzungsbefugnis des § 249 Abs. 2 BGB die Veräußerung seines beschädigten
+            Kraftfahrzeugs grundsätzlich zu demjenigen Preis vornehmen darf, den ein von ihm eingeschalteter
+            unabhängiger Sachverständiger als Wert auf dem allgemeinen Markt ermittelt hat. Auf höhere Ankaufpreise
+            spezieller Restwertaufkäufer muss der Geschädigte sich in aller Regel nicht verweisen lassen.
+          </p>
+          <br />
+          <p>
+            Den Restwert ermittelt ein unabhängiger Sachverständiger unter Berücksichtigung des konkreten
+            Schadenbildes und regionaler Marktgegebenheiten. (siehe auch BVSK-Restwertrichtlinie)
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Wertminderung (merkantiler Minderwert)
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Der Minderwert ist ein erstattungsfähiger Schaden, der damit begründet wird, dass ein
+            Unfallwagen im Falle eines späteren Verkaufs einen geringeren Erlös erzielen kann,
+            als Fahrzeuge ohne Vorschäden.
+          </p>
+          <br />
+          <p>
+            Der Minderwert wird durch einen unabhängigen Sachverständigen im Gutachten gesondert ausgewiesen.
+          </p>
+          <br />
+          <p>
+            Auch bei älteren Fahrzeugen kann ein merkantiler Minderwert anfallen.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          130 %-Grenze
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Übersteigen die Reparaturkosten den Wiederbeschaffungswert um bis zu 30%, kann der Geschädigte
+            das Fahrzeug dennoch instandsetzen lassen, soweit er das Fahrzeug weiter nutzt und die Reparatur
+            fachgerecht durchgeführt wird.
+          </p>
+        </div>
+      </div>
+    </p>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+      <div class="collapse bg-white">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-black">
+          Fiktive Abrechnung
+        </div>
+        <div class="collapse-content text-black">
+          <p>
+            Der Geschädigte kann gemäß § 249 BGB frei wählen, ob er das Fahrzeug instandsetzen lässt oder ob er
+            sich die ermittelten Reparaturkosten auszahlen lässt (fiktive Abrechnung). Liegen die Reparaturkosten
+            oberhalb von 70%, wird bei der fiktiven Abrechnung nach herrschender Rechtsprechung der Restwert in Abzug
+            gebracht
+          </p>
+          <p>
+            (Wiederbeschaffungswert abzüglich Restwert = Entschädigungsbetrag).
+          </p>
+          <br />
+          <p>
+            Der Geschädigte darf in diesen Fällen sein beschädigtes Fahrzeug zu dem Wert veräußern, den der
+            Sachverständige als Wert auf dem allgemeinen regionalen Markt ermittelt hat.
+          </p>
+        </div>
+      </div>
+    </p>
+  </article>
+  <div class="ml-32 pt-10">
+    <div class="card card-compact w-96 bg-white shadow-xl">
+      <div class="card-body">
+        <div class="w-8 h-8 items-center justify-center">
+          <img src="/images/telefonIcon.png" alt="Telefon Icon"  />
+        </div>
+        <h3 class="card-title text-xl text-black">Telefon:</h3>
+        <p class="text-xl text-black">02104/5043939</p>
+        <br />
+        <div class="w-8 h-8 items-center justify-center">
+          <img src="/images/handyIcon.png" alt="Handy Icon" />
+        </div>
+        <h3 class="card-title text-xl text-black">Mobil:</h3>
+        <p class="text-xl text-black">0172 9999 006</p>
+      </div>
+      <figure>
+        <img src="/images/damianDraussen.jpg" alt="Felge" class="w-full"/>
+      </figure>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
