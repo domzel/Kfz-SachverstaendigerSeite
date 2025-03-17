5fad7a4388d56ebb977dafbc9d484d0b7aac2bb5
Dominik Zelder <dominik.zelder@stud.htwk-leipzig.de>
2025-03-17 17:30:15 +0100

    refactor(): Seite Lakierfehler weiter gemacht.

--- KfzSeite/pages/unfall/lackierfehler.vue
+++ KfzSeite/pages/unfall/lackierfehler.vue
@@ -1,5 +1,56 @@
 <template>
-<p>Lackierfehler</p>
+  <section class="container mx-auto mt-10 flex flex-col lg:flex-row items-start gap-10 p-5 max-w-screen-lg">
+    <article class="flex-1">
+      <h2 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-center text-black">
+        Lakierfehler
+      </h2>
+      <br />
+      <div class="bg-white shadow-lg rounded-lg p-6 w-full">
+        <div class="text-black">
+          <p>
+            Eine Reparaturlackierung wird immer genauer in Augenschein genommen als die Werkslackierung beim Autokauf.
+            Bei einer Lackierung können immer wieder Fehler auftreten. Selbst wenn die Produkte und Werkzeuge,
+            mit denen täglich gearbeitet wird die höchsten Qualitätsansprüche erfüllen. Ich als Kfz-Sachverständige,
+            Gutachter kann Ihnen helfen diese Fehler zu erkennen und entsprechend zu beurteilen. Reklamationen nach
+            einer Reparaturlackierung sind immer unangenehm für beide Seiten.
+          </p>
+          <br />
+          <p>
+            Fehlerhafte Lackierung kann viele Uhrsachen haben, wie mangelnde bzw. falsche Vorbereitung des
+            Untergrundes, ein nicht sorgsamer Umgang mit den Lackmaterialien, sowie der Einsatz von minderwertigen
+            Lackmaterialien die nicht aufeinander abgestimmt worden sind. Genau wie die Arbeitsbedingungen:
+            Luftfeuchtigkeit, Trocknungszeiten und -temperatur, aufgetragene Schichtdicke usw.
+          </p>
+          <br />
+          <p>
+            Bei Farbtonabweichung kann man nicht immer von einem Verarbeitungsfehler sprechen. Die
+            Verarbeitungsweise und die Arbeitsmaterialien die bei der Werkslackierung verwendet werden
+            unterscheiden sich von denen die bei einer Reparaturlackierung den Werkstätten zu Verfügung stehen.
+          </p>
+        </div>
+      </div>
+    </article>
+    <div class="w-96 flex-shrink-0">
+      <div class="card card-compact bg-white shadow-xl">
+        <div class="card-body">
+          <div class="w-8 h-8 items-center justify-center">
+            <img src="/images/telefonIcon.png" alt="Telefon Icon" />
+          </div>
+          <h3 class="card-title text-xl text-black">Telefon:</h3>
+          <p class="text-xl text-black">02104/5043939</p>
+          <br />
+          <div class="w-8 h-8 items-center justify-center">
+            <img src="/images/handyIcon.png" alt="Handy Icon" />
+          </div>
+          <h3 class="card-title text-xl text-black">Mobil:</h3>
+          <p class="text-xl text-black">0172 9999 006</p>
+        </div>
+        <figure>
+          <img src="/images/damianDraussen.jpg" alt="Felge" class="w-full" />
+        </figure>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
